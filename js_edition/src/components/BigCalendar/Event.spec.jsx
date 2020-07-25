b290c61ee1364e90514ef00757ad14c6517d22c4
Rafael Weiss <dataweiss@gmail.com>
2020-07-25 18:08:49 -0300

    Adding tests for event component

---
+++ js_edition/src/components/BigCalendar/Event.spec.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import moment from "moment";
+
+import Event from "../BigCalendar/Event";
+
+describe("<Event />", () => {
+  it("should match snapshot when is sickness", () => {
+    const event = {
+      id: 1,
+      title: "Mike",
+      allDay: true,
+      type: "sickness",
+      start: moment("2017-01-01"),
+      end: moment("2017-01-01"),
+    };
+    const { container } = render(<Event event={event} />);
+    expect(container).toMatchSnapshot();
+  });
+  it("should match snapshot when is on vacation", () => {
+    const event = {
+      id: 2,
+      title: "Tom",
+      allDay: true,
+      type: "vacation",
+      start: moment("2017-01-02"),
+      end: moment("2017-01-04"),
+    };
+    const { container } = render(<Event event={event} />);
+    expect(container).toMatchSnapshot();
+  });
+});
