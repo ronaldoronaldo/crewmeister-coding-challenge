65ef9e21a49ec1a1c596f78ebce3da09e2720216
Rafael Weiss <dataweiss@gmail.com>
2020-07-26 15:59:38 -0300

    Creating Toolbar test

--- js_edition/src/components/BigCalendar/Toolbar/Toolbar.spec.jsx
+++ js_edition/src/components/BigCalendar/Toolbar/Toolbar.spec.jsx
@@ -2,31 +2,11 @@ import React from "react";
 import { render } from "@testing-library/react";
 import moment from "moment";
 
-import Toolbar from "./Event";
+import Toolbar from "./Toolbar";
 
 describe("<Event />", () => {
-  it("should match snapshot when is sickness", () => {
-    const event = {
-      id: 1,
-      title: "Mike",
-      allDay: true,
-      type: "sickness",
-      start: moment("2017-01-01"),
-      end: moment("2017-01-01"),
-    };
-    const { container } = render(<Toolbar event={event} />);
-    expect(container).toMatchSnapshot();
-  });
-  it("should match snapshot when is on vacation", () => {
-    const event = {
-      id: 2,
-      title: "Tom",
-      allDay: true,
-      type: "vacation",
-      start: moment("2017-01-02"),
-      end: moment("2017-01-04"),
-    };
-    const { container } = render(<Toolbar event={event} />);
+  it("should match snapshot", () => {
+    const { container } = render(<Toolbar />);
     expect(container).toMatchSnapshot();
   });
 });
